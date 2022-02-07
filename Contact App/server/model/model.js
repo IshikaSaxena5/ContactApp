44957f09a21add56af14311880db4b66d786a11c
Ishika Saxena <ishikasaxena05032002@gmail.com>
2022-02-07 16:42:57 +0530

    first commit

---
+++ Contact App/server/model/model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    name : {
+        type : String,
+        required: true
+    },
+    phone:{
+        type : Number,
+        required : true,
+        unique : true
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true
+    }
+})
+
+const Userdb = mongoose.model('userdb', schema);
+
+module.exports = Userdb;
